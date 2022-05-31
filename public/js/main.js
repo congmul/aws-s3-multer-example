58839671bdec29c2ad6c527ca5a3e7e933f9d3b0
Jehyun <congmul@hotmail.com>
2022-05-30 17:47:36 -0700

    Update Form behavior

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
 document.getElementById('img-input-form').onchange = function(event){
-    document.getElementById('avatar').innerHTML = `<img src=${URL.createObjectURL(event.target.files[0])} width="100%" />`
+    document.getElementById('avatar').innerHTML = `
+    <img src=${URL.createObjectURL(event.target.files[0])} width="100%" />
+    <p>Preview</p>
+    <p>Uploading....</p>
+    `
+    document.getElementById("profile-form").submit();
 };
