03f416a987d9897825fb2d6f4b85bffa7fd58731
Jehyun <congmul@hotmail.com>
2022-06-01 18:38:43 -0700

    Add some style

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 document.getElementById('img-input-form').onchange = async function(event){
     document.getElementById('avatar').innerHTML = `
     <img src=${URL.createObjectURL(event.target.files[0])} width="200px" />
-    <p>Preview</p>
     `
 };
